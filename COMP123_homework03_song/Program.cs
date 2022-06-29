fd94ad2610d9b563514315fc3c0674399e8c88f5
Arvin <108429574+arvincalmario@users.noreply.github.com>
2022-06-29 16:19:21 -0400

    Add files via upload

---
+++ COMP123_homework03_song/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace COMP123_homework03_song
+{
+    class Program
+    {
+        [Flags]
+        enum SongGenre
+        {
+            Unclassified = 0,
+            Pop = 0b1,
+            Rock = 0b10,
+            Blues = 0b100,
+            Country = 0b1_000,
+            Metal = 0b10_000,
+            Soul = 0b100_000
+        }
+        static void Main(string[] args)
+        {
+            //To test the constructor and the ToString method
+            Console.WriteLine(new Song("Baby", "Justin Bebier", 3.35, SongGenre.Pop));
+
+            //This is first time that you are using the bitwise or. It is used to specify a combination of genres
+            Console.WriteLine(new Song("The Promise", "Chris Cornell", 4.26, SongGenre.Country | SongGenre.Rock));
+
+            Library.LoadSongs("Week_03_lab_09_songs4.txt");     //Class methods are invoke with the class name
+            Console.WriteLine("\n\nAll songs");
+            Library.DisplaySongs();
+
+            SongGenre genre = SongGenre.Rock;
+            Console.WriteLine($"\n\n{genre} songs");
+            Library.DisplaySongs(genre);
+
+            string artist = "Bob Dylan";
+            Console.WriteLine($"\n\nSongs by {artist}");
+            Library.DisplaySongs(artist);
+
+            double length = 5.0;
+            Console.WriteLine($"\n\nSongs more than {length}mins");
+            Library.DisplaySongs(length);
+
+        }
+        class Song
+        {
+            public string Title { get; }
+            public string Artist { get; }
+            public double Length { get; }
+            public SongGenre Genre { get; }
+            public Song(string title, string artist, double length, SongGenre genre)
+            {
+                Title = title;
+                Artist = artist;
+                Length = length;
+                Genre = genre;
+            }
+            public override string ToString()
+            {
+                return $"{Title} by {Artist} ({Genre}) {Length}min  \n\n ";
+            }
+        }
+        static class Library
+        {
+            private static List<Song> songs = new List<Song>();
+            //public static void LoadSongs(string filename)
+            //{
+            //    filename = "Week_03_lab_09_songs4.txt";
+
+            //    TextReader reader = new StreamReader(filename);
+            //    string line = reader.ReadLine();
+
+
+            //    while (line != null)
+            //    {
+            //        //Console.WriteLine(line);
+            //        //line = reader.ReadLine();
+
+            //        string title = line;
+            //        string artist = line;
+            //        double length = Convert.ToDouble(line);
+            //        SongGenre genre = (SongGenre)Enum.Parse(typeof(SongGenre), line);
+
+            //        Song songs = new Song(title, artist, length, genre);
+
+            //        // i keep getting an exception error on this block of code professor so i cant finish the whole homework. im stuck
+            //    }
+
+            //    reader.Close();
+            //}
+            public static void LoadSongs(string fileName)
+            {
+                TextReader reader = new StreamReader(fileName);
+                songs = new List<Song>();
+                string line = reader.ReadLine();
+                string artist;
+                string title;
+                double length;
+                SongGenre genre;
+                while ((title = reader.ReadLine()) != null)
+                {
+
+                    artist = line;
+                    length = Convert.ToDouble(reader.ReadLine());
+                    genre = (SongGenre)Enum.Parse(typeof(SongGenre), reader.ReadLine());
+                    songs.Add(new Song(artist, title, length, genre));
+                    line = reader.ReadLine();
+                }
+
+                reader.Close();
+            }
+
+            public static void DisplaySongs()
+            {
+
+            }
+            public static void DisplaySongs(double longerThan)
+            {
+
+            }
+            public static void DisplaySongs(SongGenre genre)
+            {
+
+            }
+            public static void DisplaySongs(string artist)
+            {
+
+            }
+        }
+    }
+}
---
+++ COMP123_homework03_song/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("COMP123_homework03_song")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("COMP123_homework03_song")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ba1929ec-5eba-4fd4-a294-bd3bf3f43f3f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
